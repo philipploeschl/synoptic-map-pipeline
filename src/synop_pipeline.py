070b1a9ebdaddeb3c0ae8488deb52b980ca30309
Philipp Loeschl <loeschl@mpsmpg.de>
2025-08-13 17:56:01 +0200

    added argument parsing

--- src/synop_pipeline.py
+++ src/synop_pipeline.py
@@ -1,41 +1,63 @@
 import os
 import subprocess
-import config
-from misc import run_all_scripts, get_current_session_folder
-#from los.drms_prep import main as drms_prep_main
+import argparse
 
-# ToDo:
-# - Move session creation logic into this file and replace python file calls with direct function calls
-#   - python synop_pipeline.py --config=/path/to/config.py -> creates new session folder and saves the config into that folder
-#   - python synop_pipeline.py --session=/path/to/session_folder/ loads a previous session and uses the config from that folder
-#   - python synop_pipeline.py without command line arguments will default to config.py in sim/los/
-# - load config in synop_pipeline.py and pass it to sub routine calls along with the session folder
-# - direct execution of python sub routines will continued to be supported via their respective "if __name__ = "__main__":" structures
-# - session_path.txt obsolete due to replacing inter-script communication with direct calls
-# - ./script/ output cleanup advisable for repeated processing in a single session
-# - split up config.py into pipeline speciifc pipeline_config.py and Carrington rotation specific session_config.py?
-#   - possibly only for previous session calls where configs are loaded from /session_path/config/
+from src.utils.utils import run_all_scripts, get_current_session_folder
+from los.drms_preparation import main as drms_main
+from los.m720s_drms_pipe import main as hmi_data_main
+from los.phi_drms_interface import main as phi_data_main
+from los.hmiphisynoptic import main as synop_main
+
+from config.config import Config
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run the synoptic pipeline with optional config and session paths."
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to config.yaml (uses default parameters if no config is provided)."
+    )
+    parser.add_argument(
+        "--session",
+        type=str,
+        help="Path to an existing session folder (creates new session if no folder is provided)."
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-
     #set cwd to file directory
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+    args = parse_args()
+
+    # Determine session folder
+    if args.session:
+        session_folder = os.path.abspath(args.session)
+    else:
+        session_folder = get_current_session_folder() # TODO CHANGE SO IT LOADS PROVIDED SESSION
+
+    # Load config from YAML
+    config = Config(config_path=args.config) if args.config else Config()
+
+
     if config.run_drms_prep:
         if config.verbose: print("Running 0_drms_prep.py ...")
-        subprocess.call(['python', 'los/0_drms_prep.py'])
-        #drms_prep_main.main()#config, session_folder)
+        #subprocess.call(['python', 'los/0_drms_prep.py'])
+        drms_main(config, session_folder)
 
     if config.run_m720s_drms_pipe:
         if config.verbose: print("Running 1_m720s_drms_pipe.py ...")
-        subprocess.call(['python', 'los/1_m720s_drms_pipe.py'])
-        # 1_m720s.drms_pipe.main(config, session_folder)
+        #subprocess.call(['python', 'los/1_m720s_drms_pipe.py'])
+        hmi_data_main(config, session_folder)
 
     if config.run_phi_drms_interface:
         if config.verbose: print("Running 2_phi_drms_interface.py ...")
-        subprocess.call(['python', 'los/2_phi_drms_interface.py'])  
-        # 2_phi_drms_interface.main(config, session_folder)
+        #subprocess.call(['python', 'los/2_phi_drms_interface.py'])  
+        phi_data_main(config, session_folder)
 
     # This will run all / only phi/ only hmi scripts in the outpath_scripts directory 
     if config.run_hmi_scripts and config.run_phi_scripts:
@@ -48,7 +70,7 @@ if __name__ == "__main__":
     if config.run_hmiphisynoptic:
         if config.verbose: print("Running 3_hmiphisynoptic.py ...")
         subprocess.call(['python', 'los/3_hmiphisynoptic.py'])
-        # 3_hmiphisynoptic.main(config, session_folder)
+        synop_main(config, session_folder)
 
     # Todo
     #if config.run_polefilling:
