8d3e3c8803abe0ca9687439c3482d5c70b317c1b
Philipp Loeschl <loeschl@mpsmpg.de>
2025-08-20 17:43:59 +0200

    fixed run_bash_script session_folder handling

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -112,11 +112,10 @@ def make_handle_sigint(stop_signal_path):
     return handle_sigint
 
 
-def run_bash_scripts(config, verbose=False, prefix=''):
+def run_bash_scripts(config, session_folder, verbose=False, prefix=''):
 
     if verbose: print("Running DRMS bash scripts...")
 
-    session_folder = get_current_session_folder()
     outpath_scripts = os.path.join(session_folder, config.script_path)
     outpath_logs    = os.path.join(session_folder, config.log_path)
     
