de29fe3ed0983f9ff5c6a51bc544f7845140b574
Philipp Loeschl <loeschl@mpsmpg.de>
2025-11-05 20:01:15 +0100

    fixed diagnostic pdf output filename

--- src/synop/los/hmiphisynoptic.py
+++ src/synop/los/hmiphisynoptic.py
@@ -1428,7 +1428,7 @@ def main(global_config, session_folder):
     hdul = fits.HDUList([hdu, table_hdu])
     hdul.writeto(os.path.join(synop_outpath,config['synop_name']), overwrite=True)
 
-    diagnostics(synop_img, table_hdu.data, synop_outpath, config['synop_name'][:-5]+"_diagnostics", global_config)
+    diagnostics(synop_img, table_hdu.data, synop_outpath, config['synop_name'][:-5], global_config)
     plot_synoptic_sources(synop_img, synop_outpath, config['synop_name'][:-5], global_config, table_hdu.data, pdf=True) # cut out .fits
     
     if config["bin"]:
