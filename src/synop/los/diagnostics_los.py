a0c94a1eb076ffe37cd93a2513c5f8302e66ecb4
Philipp Loeschl <loeschl@mpsmpg.de>
2026-02-11 16:56:59 +0100

    added new pipeline modules

---
+++ src/synop/los/diagnostics_los.py
@@ -0,0 +1,18 @@
+import os
+from astropy.io import fits
+from utils.diagnostics import diagnostics
+
+STATUS_OK = 0
+
+def main(config, session_folder):
+  
+    synop_outpath = os.path.join(session_folder, config.synop_path)
+    synop_hdul = fits.open(synop_outpath + config.synop_name)
+
+    synop_img  = synop_hdul[0].data
+    table_data = synop_hdul[1].data
+    carrington_number = synop_hdul[0].header['CAR_ROT']
+    
+    diagnostics(synop_img, table_data, synop_outpath, carrington_number, config)
+    
+    return STATUS_OK
